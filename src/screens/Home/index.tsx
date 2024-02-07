221df02048e30a0b8ebaed3258dd109570b823a8
adrielhenry33 <86744909+adrielhenry33@users.noreply.github.com>
2024-02-07 17:56:16 -0300

    fazendo o plano de fundo

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,16 @@
+
+import { View, Text } from 'react-native';
+
+import { styles } from './styles';
+
+
+
+export default function Home() {
+    return(
+        <View style = {styles.container}>
+            <Text style ={styles.appName}>
+                Developing app.
+            </Text>
+        </View>
+    );
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create ({
+
+    container:{
+        backgroundColor: '#0D0D0D',
+        flex: 1
+    },
+    appName:{
+        color: '#FFF',
+        fontSize: 20,
+        textAlign: 'center',
+        paddingTop: 25,
+    }
+
+});
